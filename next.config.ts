5305c7c0bee2e6cf5c557e4faaf9a83d52a1503d
stonoli <stonolidm@gmail.com>
2025-03-13 17:01:22 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  webpack: (config) => {
+    // Alias pour forcer l'utilisation du module générique
+    config.resolve.alias['@ffmpeg-installer/linux-x64'] =
+      require.resolve('@ffmpeg-installer/ffmpeg');
+    return config;
+  },
 };
 
 export default nextConfig;
