b4b4f88ab3bf09501ba4596ca464d66d9c9d56ed
stonoli <stonolidm@gmail.com>
2025-03-13 15:34:18 +0100

    json vercel

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // S'assurer que config.externals est un tableau
-      config.externals = Array.isArray(config.externals)
-        ? config.externals
-        : [config.externals];
-      // Ajouter ffmpeg‑static en tant qu'external
-      config.externals.push({ 'ffmpeg-static': 'commonjs ffmpeg-static' });
-    }
-    return config;
-  },
+
 };
 
 export default nextConfig;
