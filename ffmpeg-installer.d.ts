b7f98a00283c36463b9f6d6565e584d47c656f13
stonoli <stonolidm@gmail.com>
2025-03-13 15:58:01 +0100

    fix type import

---
+++ ffmpeg-installer.d.ts
@@ -0,0 +1,11 @@
+declare module '@ffmpeg-installer/linux-x64' {
+    export const path: string;
+    const defaultExport = { path };
+    export default defaultExport;
+}
+  
+declare module '@ffmpeg-installer/ffmpeg' {
+    export const path: string;
+    const defaultExport = { path };
+    export default defaultExport;
+}
