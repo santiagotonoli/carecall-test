4617bfcae4a2fb037383bd52bb8c7fb16507be71
stonoli <stonolidm@gmail.com>
2025-03-14 02:55:00 +0100

    cors pb

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Configure multer pour enregistrer temporairement les fichiers dans /tmp
 const storage = multer.diskStorage({
